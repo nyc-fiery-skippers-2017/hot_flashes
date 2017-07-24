ea67a3a271428de5001f457c93c4eac1a5244484
Sara Sulit <email": null>
2017-07-24 10:58:46 -0400

    fix errors

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -3,10 +3,10 @@ get '/rounds/:rounds_id/cards/:id' do
   @round = Round.find_by(id: params[:rounds_id])
   @deck = Deck.find_by(id: @round.deck_id)
   @card = Card.find_by(id: params[:id])
-  if @round.card.count == @round.correct_cards.count
+  if @round.cards.count == @round.correct_cards.count
     redirect "/rounds/#{@round.id}"
   else
-    erb :'cards/show'
+    erb :'/cards/show'
   end
 end
 
